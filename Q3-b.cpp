9bbb58793264645c10017b353cdd37372203a99b
subhamk09 <shubhamk09112006@gmail.com>
2025-02-08 21:39:22 +0530

    Add files via upload

---
+++ Q3-b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    int left = 0, right = str.length() - 1;
+    bool isPalindrome = true;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            isPalindrome = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (isPalindrome) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
